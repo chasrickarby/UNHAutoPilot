0bdcae3864b6857e5d9f1c59d7594f55d145c140
DB56 <dac67@wildcats.unh.edu>
2016-09-15 11:35:10 -0400

    First commit

---
+++ AutonomousCar/src/AutonomousCar/AutonomousControl.java
@@ -0,0 +1,147 @@
+package AutonomousCar;
+
+import java.io.*;
+
+import java.lang.Math;
+import java.lang.Double;
+import java.awt.*;
+
+import SimConnection.*;
+import SimUtilities.*;
+
+/******************************************************
+ * This contains the procedures to get control related feedback
+ * from the simulator, and to perform control computations
+ * based on that feedback.
+ *
+ *@author Tom Miller
+ *@version 1.0
+ *@since 9/02/2016
+ */
+public class AutonomousControl {
+
+    private SIMConnection simConnection;
+    private SIMHeading heading;
+    private SIMVelocity velocity;
+    private SIMAccelPedal gaspedal;
+    private SIMBrakePedal brakepedal;
+    private SIMSteeringWheel steeringwheel;
+    private SIMLanePosition laneposition;
+
+    private SIMDisplayText velDisplay;
+    private SIMDisplayText posDisplay;
+    private SIMDisplayText strDisplay;
+
+    private double speedmph = 0.0;
+    private double laneposmeters = 0.0;
+    private double oldlanepos = 0.0;
+    private boolean inlane = true;
+    private boolean autopilot = true;
+    private double steerangle = 0.0;
+
+    /*******************************************
+     * The constructor
+     *
+     * @param sc The simulator connection object
+     ********************************************/
+    public AutonomousControl(SIMConnection sc) {
+        simConnection = sc;
+        // feedback items
+        heading = new SIMHeading(simConnection);
+        velocity = new SIMVelocity(simConnection);
+        laneposition = new SIMLanePosition(simConnection);
+        // actuators
+        gaspedal = new SIMAccelPedal(simConnection);
+        brakepedal = new SIMBrakePedal(simConnection);
+        steeringwheel = new SIMSteeringWheel(simConnection);
+        // display items
+        velDisplay = new SIMDisplayText(simConnection, "velocity");
+        velDisplay.position(0.1, 0.1);
+        velDisplay.color(Color.white);
+        velDisplay.size(1.5);
+        velDisplay.channel(2);
+        posDisplay = new SIMDisplayText(simConnection, "lanepos");
+        posDisplay.position(0.1, 0.2);
+        posDisplay.color(Color.white);
+        posDisplay.size(1.5);
+        posDisplay.channel(2);
+        strDisplay = new SIMDisplayText(simConnection, "steering");
+        strDisplay.position(0.3, 0.1);
+        strDisplay.color(Color.blue);
+        strDisplay.size(1.5);
+        strDisplay.channel(2);
+    }
+
+    /*******************************************
+     * This method queues the commands needed to
+     * get the feedback items desired for the
+     * control algorithm.
+     ********************************************/
+    public void getFeedback() {
+        heading.get();
+        velocity.get();
+        laneposition.get();
+    }
+
+    /*******************************************
+     * This method determines the control actions
+     * based on the feedback, and queues the
+     * simulator commands needed to make those
+     * actions happen.
+     ********************************************/
+    public void doControl() {
+        // grab the feedback values
+        speedmph = velocity.mph();
+        inlane = laneposition.inlane();
+        laneposmeters = laneposition.value();
+
+        // do control if in lane
+        if (inlane) {
+            // do steering control (if moving)
+            if (speedmph > 1.0) {
+                // proportionl control
+                steerangle = -100 * laneposmeters;
+                // differential control
+                steerangle += -30 *(laneposmeters - oldlanepos) * speedmph;
+                oldlanepos = laneposmeters;
+                // set the new steering angle
+                steeringwheel.set(steerangle);
+                autopilot = true;
+            }
+            // do brake and gas pedal control
+            // (not yet supported)
+        } else {
+            // if not in lane, turn off autopilot
+            if (autopilot) {
+                steeringwheel.release();
+                brakepedal.release();
+                gaspedal.release();
+                autopilot = false;
+            }
+        }
+    }
+
+    /*******************************************
+     * This method displays useful info on
+     * the simulator screen.
+     ********************************************/
+    public void doDisplay() {
+        String vels = Double.toString(speedmph);
+        String lpos = Double.toString(laneposmeters);
+        String sangle = Double.toString(steerangle);
+        // display velocity
+        velDisplay.set(vels.substring(0, vels.indexOf('.')));
+        // display lane position
+        if (inlane) {
+            posDisplay.set(lpos.substring(0, lpos.indexOf('.') + 2));
+        } else {
+            posDisplay.set("---");
+        }
+        // display autopilot steering angle
+        if (autopilot) {
+            strDisplay.set(sangle.substring(0, sangle.indexOf('.')));
+        } else {
+            strDisplay.set("---");
+        }
+    }
+}
---
+++ AutonomousCar/src/AutonomousCar/Main.java
@@ -0,0 +1,80 @@
+package AutonomousCar;
+
+import java.io.*;
+
+import java.lang.Math;
+import java.lang.Double;
+import java.awt.*;
+
+import SimConnection.*;
+import SimUtilities.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        //Set the port that the server will open and use for TCP communications
+        SIMConnection simConnection = new SIMConnection(4444);
+
+        // Define the controller object
+        AutonomousControl autoControl = new AutonomousControl(simConnection);
+
+        boolean synched = true;
+
+        System.out.println("Starting Server\n");
+
+        // wait here for simulator to connect
+        if (!simConnection.acceptConnection()) {
+            System.err.println("TCP connection to simulator failed!");
+            System.err.println(simConnection.getMessage());
+            return;
+        }
+        System.out.println("Connection Established");
+
+        // set the control update rate
+        simConnection.changeRate(30);
+
+        // start timers for pacing things
+        Timer t100 = new Timer(100);
+        Timer t1000 = new Timer(1000);
+
+        // the main control loop
+        while (true) {
+
+            // update the periodic timers once each cycle
+            t100.update();
+            t1000.update();
+
+            // get feedback for control
+            autoControl.getFeedback();
+
+            // communicate/synchronize with the simulator
+            if (!simConnection.synchSimulator()) {
+                System.err.println("TCP exchange with simulator failed!");
+                System.err.println(simConnection.getMessage());
+                return;
+            } else {
+                // verify synchronization with simulator OK
+                if (simConnection.checkSynch() != 1) {
+                    synched = false;
+                }
+                // display the synch error 1 times per second
+                if (t1000.timeout()) {
+                    if (!synched) {
+                        synched = true;
+                        System.err.println("WARNING: Simulation synchronization failure");
+                    }
+                }
+            }
+
+            // compute next control actions based on feedback
+            autoControl.doControl();
+
+            // display controller state 10 times per second
+            if (t100.timeout()) {
+                autoControl.doDisplay();
+            }
+
+        }
+    }
+}
